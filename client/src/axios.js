c16f52847f4cb757241a57a5d9dcb465dea9c415
Anamya Vats <121956516+Anamya01@users.noreply.github.com>
2024-06-14 18:56:24 +0530

    Update axios.js

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const url = "https://api.themoviedb.org/3/";
-const api = "5d4795d808889e47fb760dab465b15e7"
+const api = "YOUR TMDB API KEY"
 
 const endpoints = {
     orignals : "discover/tv",
